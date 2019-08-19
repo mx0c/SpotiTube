9dec1c4e47cde10b9a65ed7cf7020e339277b91c
mx0c <marius-bauer-2006@web.de>
2019-08-19 18:10:39 +0200

    refactored xaml, removed ffmpeg, added new player functionalities

---
+++ MainPage.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using SpotiTube;
+using Windows.Media.Playback;
+
+namespace App1
+{
+    public sealed partial class MainPage : Page
+    {
+        private MusicPlayer musicPlayer;
+        public MainPage()
+        {
+            this.InitializeComponent();
+            this.musicPlayer = new MusicPlayer(this.CurrentDuration, this.CurrentTime, this.TimeSlider);
+            this.TimeSlider.AddHandler(PointerReleasedEvent, new PointerEventHandler(TimeSlider_OnPointerRelease),true);
+        }
+
+        public void changeCurrentDurationText(String currDur)
+        {
+            this.CurrentDuration.Text = currDur;
+        }
+
+        public void changeCurrentTimeText(String currTime)
+        {
+            this.CurrentTime.Text = currTime;
+        }
+
+        private void LeftButton_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void RightButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void PlayButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.musicPlayer.mPlayer.CurrentState == MediaPlayerState.Stopped || this.musicPlayer.mPlayer.CurrentState == MediaPlayerState.Closed)
+            {
+                this.PlayButton.Content = "\uE769";
+                this.musicPlayer.PlaySong(this.SearchBar.Text);
+            }
+            else if (this.musicPlayer.mPlayer.CurrentState == MediaPlayerState.Paused)
+            {
+                this.PlayButton.Content = "\uE769";
+                this.musicPlayer.resume();
+            }
+            else
+            {
+                this.PlayButton.Content = "\uE768";
+                this.musicPlayer.PauseSong();
+            }
+        }
+
+        private void VolumeSlider_Changed(object sender, RoutedEventArgs e) {
+            try
+            {
+                this.musicPlayer.adjustVolume(this.VolumeSlider.Value);
+            }
+            catch (NullReferenceException)
+            {
+                return;
+            }
+        }
+
+        private void TimeSlider_OnPointerRelease(object sender, PointerRoutedEventArgs e)
+        {
+            this.musicPlayer.seek(this.TimeSlider.Value);
+        }
+    }
+}
---
+++ MusicPlayer.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YoutubeExplode;
+using YoutubeExplode.Converter;
+using YoutubeExplode.Models.MediaStreams;
+using Windows.Media.Core;
+using Windows.Media.Playback;
+using Windows.UI.Xaml.Controls;
+using System.Timers;
+using Windows.UI.Core;
+
+namespace SpotiTube
+{
+    class MusicPlayer
+    {
+        public int currentSongDuration { get; set; }
+        public int currentSongTime { get; set; }
+        public MediaPlayer mPlayer = new MediaPlayer();
+        private Timer clock = new Timer();
+
+        private TextBlock currentDuration;
+        private TextBlock currentTime;
+        private Slider timeSlider;
+
+        public MusicPlayer(TextBlock CurrentDuration, TextBlock CurrentTime, Slider TimeSlider) {
+            this.currentDuration = CurrentDuration;
+            this.currentTime = CurrentTime;
+            this.timeSlider = TimeSlider;
+            this.clock.Elapsed += new ElapsedEventHandler(onTimerEvent);
+            this.clock.Interval = 1000;
+            this.clock.Enabled = true;
+        }
+
+        public async void onTimerEvent(object source, ElapsedEventArgs e) {
+            if (this.mPlayer.CurrentState == MediaPlayerState.Playing)
+            {
+                await Windows.ApplicationModel.Core.CoreApplication.MainView.CoreWindow.Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+                { 
+                    String sec = this.mPlayer.Position.Seconds >= 10 ? this.mPlayer.Position.Seconds.ToString() : "0" + this.mPlayer.Position.Seconds.ToString();
+                    this.currentTime.Text = $"{this.mPlayer.Position.Minutes}:{sec}";
+                    sec = this.mPlayer.NaturalDuration.Seconds >= 10 ? this.mPlayer.NaturalDuration.Seconds.ToString() : "0" + this.mPlayer.NaturalDuration.Seconds.ToString();
+                    this.currentDuration.Text = $"{this.mPlayer.NaturalDuration.Minutes}:{sec}";
+                    this.timeSlider.Value = this.mPlayer.Position.TotalSeconds / (this.mPlayer.NaturalDuration.TotalSeconds / 100.0);
+                });
+            }
+        }
+
+        public async Task<String> GetAudioStream(String youtubeURL)
+        {
+            var client = new YoutubeClient();
+            var converter = new YoutubeConverter(client);
+            var mediaStreamInfoSet = await client.GetVideoMediaStreamInfosAsync(youtubeURL.Split('=')[1]);
+            var audioStreamInfo = mediaStreamInfoSet.Audio.WithHighestBitrate();
+            return audioStreamInfo.Url;
+        }
+
+        public void PauseSong()
+        {
+            this.mPlayer.Pause();
+        }
+
+        public void resume() {
+            this.mPlayer.Play();
+        }
+
+        public void adjustVolume(double percentage)
+        {
+            this.mPlayer.Volume = percentage / 100.0;
+        }
+
+        public async void PlaySong(String youtubeURL, bool debugging = false)
+        {
+            var stream = await this.GetAudioStream(youtubeURL); 
+            this.mPlayer.Source = MediaSource.CreateFromUri(new Uri(stream));      
+            this.mPlayer.Play();
+        }
+
+        public void seek(double percentage)
+        {
+            this.mPlayer.Position = this.mPlayer.NaturalDuration.Multiply(percentage / 100);
+        }
+
+    }
+}
